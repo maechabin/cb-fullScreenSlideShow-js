e7e950d422339c84178093651bfead936b5183e4
maechabin <mail@chab.in>
2015-10-29 01:38:41 +0900

    test

--- test/test.js
+++ test/test.js
@@ -5,12 +5,30 @@
   QUnit.module("fullscreen", {
     beforeEach: function () {
       this.fsss = $(".cb-fsss");
+      this.fsss.cbFullScreenSlideShow({
+        img: [
+          "http://jsrun.it/assets/U/z/W/x/UzWxP.jpg",
+          "http://jsrun.it/assets/k/I/5/n/kI5nR.jpg",
+          "http://jsrun.it/assets/g/5/i/7/g5i7d.jpg"
+        ]
+      });
     }
   });
 
   QUnit.test("$.fn.cbFullScreenSlideShow()が読み込まれているか", function (assert) {
-    this.fsss.cbFullScreenSlideShow();
     assert.ok($.fn.cbFullScreenSlideShow, "Passed!!");
+  });
+
+  QUnit.test("デフォルト値のチェック", function (assert) {
+
+    var width = $(window).width();
+    var height = $(window).height();
+
+    assert.equal(this.fsss.css("z-index"), 999, "Passed!!");
+    assert.equal(this.fsss.css("background-color"), "rgba(1, 1, 1, 0)", "Passed!!");
+    assert.equal(this.fsss.css("width"), width + "px", "Passed!!");
+    assert.equal(this.fsss.css("height"), height + "px", "Passed!!");
+
   });
 
 } (jQuery, QUnit, window, document));
